969893ccbad9c1c6f5bd0db73e803e7428521b32
RandaeGS <garciasanchezrd@gmail.com>
2024-11-08 17:51:43 -0400

    Create materialApp

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,17 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Pokedex Flutter",
+      theme: ThemeData(
+        primarySwatch: Colors.red
+      ),
+    );
+  }
+}
