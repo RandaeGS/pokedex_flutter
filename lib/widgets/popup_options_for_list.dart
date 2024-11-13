92d24dc53e4cf0fe8a9e19b126f26fa2b2d4db55
RandaeGS <garciasanchezrd@gmail.com>
2024-11-13 19:08:36 -0400

    init widget that shows options

---
+++ lib/widgets/popup_options_for_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PopupOptionsForList extends StatefulWidget {
+  const PopupOptionsForList({super.key});
+
+  @override
+  State<PopupOptionsForList> createState() => _PopupOptionsForListState();
+}
+
+class _PopupOptionsForListState extends State<PopupOptionsForList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
