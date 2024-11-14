5f1637c7f9742f8a677e5576c4c9d9da4cbab40d
RandaeGS <garciasanchezrd@gmail.com>
2024-11-13 22:53:05 -0400

    style filter section options so that they look like pills and change colors when pressed to indicate selection

--- lib/widgets/popup_options/FilterSection.dart
+++ lib/widgets/popup_options/FilterSection.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 class FilterSection extends StatefulWidget {
   final String title;
+  final List<String> options;
 
   const FilterSection({
     super.key,
     required this.title,
+    required this.options,
   });
 
   @override
@@ -14,7 +16,7 @@ class FilterSection extends StatefulWidget {
 
 class _FilterSectionState extends State<FilterSection> {
   bool isExpanded = false;
-  
+  Set<String> selectedOptions = {};
   
   @override
   Widget build(BuildContext context) {
@@ -46,7 +48,42 @@ class _FilterSectionState extends State<FilterSection> {
               ],
             ),
           ),
-        )
+        ),
+        if(isExpanded)(
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Wrap(
+                spacing: 8.0, // espacio horizontal entre chips
+                runSpacing: 8.0, // espacio vertical entre filas
+                children: widget.options.map((option) {
+                  final isSelected = selectedOptions.contains(option);
+                  return FilterChip(
+                    label: Text(
+                      option,
+                      style: TextStyle(
+                        color: isSelected ? Colors.white : Colors.black,
+                        fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+                      ),
+                    ),
+                    selected: isSelected,
+                    showCheckmark: false,
+                    onSelected: (bool selected) {
+                      setState(() {
+                        if (selected) {
+                          selectedOptions.add(option);
+                        } else {
+                          selectedOptions.remove(option);
+                        }
+                      });
+                    },
+                    selectedColor: Colors.blue, // Color cuando está seleccionado
+                    backgroundColor: Colors.grey[200], // Color cuando no está seleccionado
+                    checkmarkColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                  );
+                }).toList(),
+              ),
+            )),
       ],
     );
   }
