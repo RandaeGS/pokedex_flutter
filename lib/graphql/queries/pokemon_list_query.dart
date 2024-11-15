ba53c20d2971310e1b2cfddd590bc9831649cf61
RandaeGS <garciasanchezrd@gmail.com>
2024-11-15 00:00:38 -0400

    get pokemon generation id on query

--- lib/graphql/queries/pokemon_list_query.dart
+++ lib/graphql/queries/pokemon_list_query.dart
@@ -11,5 +11,8 @@ query listPokemons {
     pokemon_v2_pokemonsprites {
       sprites(path: "other.official-artwork.front_default")
     }
+    pokemon_v2_pokemonspecy {
+      generation_id
+    }
   }
 }""";
