d7dc8396d2aca5ee67fc3a46a575ee5eb5cf9093
RandaeGS <garciasanchezrd@gmail.com>
2024-11-15 15:24:08 -0400

    add pokemon generation filter

--- lib/graphql/queries/pokemon_list_query.dart
+++ lib/graphql/queries/pokemon_list_query.dart
@@ -1,28 +1,39 @@
 const String queryPokemonList = """
-query listPokemons(\$types: [String!]) {
- pokemon_v2_pokemon(
-   limit: 80,
-   where: {
-     pokemon_v2_pokemontypes: {
-       pokemon_v2_type: {
-         name: {
-           _in: \$types
-         }
-       }
-     }
-   }) {
-   id
-   name
-   pokemon_v2_pokemontypes {
-     pokemon_v2_type {
-       name
-     }
-   }
-   pokemon_v2_pokemonsprites {
-     sprites(path: "other.official-artwork.front_default")
-   }
-   pokemon_v2_pokemonspecy {
-     generation_id
-   }
- }
+query listPokemons(\$types: [String!], \$generations: [Int!]) {
+pokemon_v2_pokemon(
+  limit: 80,
+  where: {
+    _and: [
+      {
+        pokemon_v2_pokemontypes: {
+          pokemon_v2_type: {
+            name: {
+              _in: \$types
+            }
+          }
+        }
+      },
+      {
+        pokemon_v2_pokemonspecy: {
+          generation_id: {
+            _in: \$generations
+          }
+        }
+      }
+    ]
+  }) {
+  id
+  name
+  pokemon_v2_pokemontypes {
+    pokemon_v2_type {
+      name
+    }
+  }
+  pokemon_v2_pokemonsprites {
+    sprites(path: "other.official-artwork.front_default")
+  }
+  pokemon_v2_pokemonspecy {
+    generation_id
+  }
+}
 }""";
