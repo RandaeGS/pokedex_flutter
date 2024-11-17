b2a3df01c37336f0e616e067b218d4d5d1c5f24e
RandaeGS <garciasanchezrd@gmail.com>
2024-11-17 18:41:51 -0400

    add animation controllers to expandable fab

--- lib/widgets/popup_options/expandable_fab.dart
+++ lib/widgets/popup_options/expandable_fab.dart
@@ -17,21 +17,43 @@ class ExpandableFab extends StatefulWidget {
   State<ExpandableFab> createState() => _ExpandableFabState();
 }
 
-class _ExpandableFabState extends State<ExpandableFab> {
+class _ExpandableFabState extends State<ExpandableFab> with SingleTickerProviderStateMixin {
   bool _open = false;
+  late final AnimationController _controller;
+  late final Animation<double> _expandAnimation;
 
   @override
   void initState() {
     super.initState();
     _open = widget.initialOpen ?? false;
+    _controller = AnimationController(
+      value: _open ? 1.0 : 0.0,
+      duration: const Duration(milliseconds: 250),
+      vsync: this,
+    );
+    _expandAnimation = CurvedAnimation(
+      curve: Curves.fastOutSlowIn,
+      reverseCurve: Curves.easeOutQuad,
+      parent: _controller,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   void _toggle() {
     setState(() {
       _open = !_open;
+      if (_open) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
