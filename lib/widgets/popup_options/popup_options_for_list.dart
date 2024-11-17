d0ddbb4c4a62d96c3a417d91234c130ef4bbe32c
RandaeGS <garciasanchezrd@gmail.com>
2024-11-17 19:33:27 -0400

    add styling to popup_option_filters borders

--- lib/widgets/popup_options/popup_options_for_list.dart
+++ lib/widgets/popup_options/popup_options_for_list.dart
@@ -21,83 +21,100 @@ class _PopupOptionsForListState extends State<PopupOptionsForList> {
       ),
       elevation: 12,
       backgroundColor: Colors.white,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(1.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              FilterSection(
-                title: "Generations",
-                options: [
-                  "Generation I",    // 1996 (Red, Blue, Yellow)
-                  "Generation II",   // 1999 (Gold, Silver, Crystal)
-                  "Generation III",  // 2002 (Ruby, Sapphire, Emerald)
-                  "Generation IV",   // 2006 (Diamond, Pearl, Platinum)
-                  "Generation V",    // 2010 (Black, White, Black 2, White 2)
-                  "Generation VI",   // 2013 (X, Y, Omega Ruby, Alpha Sapphire)
-                  "Generation VII",  // 2016 (Sun, Moon, Ultra Sun, Ultra Moon)
-                  "Generation VIII", // 2019 (Sword, Shield, Brilliant Diamond, Shining Pearl)
-                  "Generation IX",   // 2022 (Scarlet, Violet)
-                ],
-                initialSelected: widget.currentFilters,
-                onFilterChange: (filters) {
-                  widget.currentFilters['generations'] = filters;
-                  widget.onFiltersChanged(widget.currentFilters);
-                },
-              ),
-              FilterSection(
-                title: "Types",
-                options: [
-                  "Normal",
-                  "Fire",
-                  "Water",
-                  "Electric",
-                  "Grass",
-                  "Ice",
-                  "Fighting",
-                  "Poison",
-                  "Ground",
-                  "Flying",
-                  "Psychic",
-                  "Bug",
-                  "Rock",
-                  "Ghost",
-                  "Dragon",
-                  "Dark",
-                  "Steel",
-                  "Fairy"
-                ],
-                initialSelected: widget.currentFilters,
-                onFilterChange: (filters) {
-                  setState(() {
-                    widget.currentFilters['types'] = filters;
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.red.shade700,
+                width: 2
+          ),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.red.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: const Offset(0, 2)
+            )
+          ]
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(1.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FilterSection(
+                  title: "Generations",
+                  options: [
+                    "Generation I",    // 1996 (Red, Blue, Yellow)
+                    "Generation II",   // 1999 (Gold, Silver, Crystal)
+                    "Generation III",  // 2002 (Ruby, Sapphire, Emerald)
+                    "Generation IV",   // 2006 (Diamond, Pearl, Platinum)
+                    "Generation V",    // 2010 (Black, White, Black 2, White 2)
+                    "Generation VI",   // 2013 (X, Y, Omega Ruby, Alpha Sapphire)
+                    "Generation VII",  // 2016 (Sun, Moon, Ultra Sun, Ultra Moon)
+                    "Generation VIII", // 2019 (Sword, Shield, Brilliant Diamond, Shining Pearl)
+                    "Generation IX",   // 2022 (Scarlet, Violet)
+                  ],
+                  initialSelected: widget.currentFilters,
+                  onFilterChange: (filters) {
+                    widget.currentFilters['generations'] = filters;
                     widget.onFiltersChanged(widget.currentFilters);
-                  });
-                },
-              ),
-              // FilterSection(
-              //   title: "Abilities",
-              //   options: [
-              //     "Overgrow",
-              //     "Chlorophyll",
-              //     "Blaze",
-              //     "Solar Power",
-              //     "Torrent",
-              //     "Rain Dish",
-              //     "Shield Dust",
-              //     "Run Away",
-              //     "Keen Eye",
-              //     "Intimidate",
-              //     "Static",
-              //     "Lightning Rod",
-              //   ],
-              //   onFilterChange: (filters) {
-              //     selectedFilters['types'] = filters;
-              //     widget.onFiltersChanged(selectedFilters);
-              //   },
-              // )
-            ],
+                  },
+                ),
+                FilterSection(
+                  title: "Types",
+                  options: [
+                    "Normal",
+                    "Fire",
+                    "Water",
+                    "Electric",
+                    "Grass",
+                    "Ice",
+                    "Fighting",
+                    "Poison",
+                    "Ground",
+                    "Flying",
+                    "Psychic",
+                    "Bug",
+                    "Rock",
+                    "Ghost",
+                    "Dragon",
+                    "Dark",
+                    "Steel",
+                    "Fairy"
+                  ],
+                  initialSelected: widget.currentFilters,
+                  onFilterChange: (filters) {
+                    setState(() {
+                      widget.currentFilters['types'] = filters;
+                      widget.onFiltersChanged(widget.currentFilters);
+                    });
+                  },
+                ),
+                // FilterSection(
+                //   title: "Abilities",
+                //   options: [
+                //     "Overgrow",
+                //     "Chlorophyll",
+                //     "Blaze",
+                //     "Solar Power",
+                //     "Torrent",
+                //     "Rain Dish",
+                //     "Shield Dust",
+                //     "Run Away",
+                //     "Keen Eye",
+                //     "Intimidate",
+                //     "Static",
+                //     "Lightning Rod",
+                //   ],
+                //   onFilterChange: (filters) {
+                //     selectedFilters['types'] = filters;
+                //     widget.onFiltersChanged(selectedFilters);
+                //   },
+                // )
+              ],
+            ),
           ),
         ),
       ),
