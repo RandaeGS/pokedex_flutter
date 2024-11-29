0cfbedde6740f0358a57fc6d2f94de1c5dae0ed8
RandaeGS <garciasanchezrd@gmail.com>
2024-11-29 00:01:04 -0400

    add logic to sort by power in pokemon list

--- lib/widgets/list_pokemon.dart
+++ lib/widgets/list_pokemon.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
@@ -16,14 +15,13 @@ class ListPokemon extends StatefulWidget {
   final int searchNumber;
   final bool updateFilter;
 
-  const ListPokemon({
-    super.key,
-    required this.activeFilters,
-    required this.currentSort,
-    required this.searchName,
-    required this.searchNumber,
-    required this.updateFilter
-  });
+  const ListPokemon(
+      {super.key,
+      required this.activeFilters,
+      required this.currentSort,
+      required this.searchName,
+      required this.searchNumber,
+      required this.updateFilter});
 
   @override
   State<ListPokemon> createState() => _ListPokemonState();
@@ -31,7 +29,8 @@ class ListPokemon extends StatefulWidget {
 
 class _ListPokemonState extends State<ListPokemon> {
   static const _pageSize = 20;
-  final PagingController<int, dynamic> _pagingController = PagingController(firstPageKey: 0);
+  final PagingController<int, dynamic> _pagingController =
+      PagingController(firstPageKey: 0);
 
   @override
   void initState() {
@@ -61,10 +60,7 @@ class _ListPokemonState extends State<ListPokemon> {
         options = QueryOptions(
             document: gql(searchPokemonByNumberQuery),
             fetchPolicy: FetchPolicy.cacheFirst,
-            variables: {
-              'number': widget.searchNumber
-            }
-        );
+            variables: {'number': widget.searchNumber});
       } else {
         switch (widget.currentSort.field) {
           case SortField.type:
@@ -74,12 +70,16 @@ class _ListPokemonState extends State<ListPokemon> {
                 variables: {
                   'offset': pageKey * _pageSize,
                   'limit': _pageSize,
-                  'name': widget.currentSort.order == SortOrder.asc ? "asc" : "desc",
+                  'name': widget.currentSort.order == SortOrder.asc
+                      ? "asc"
+                      : "desc",
                   'types': _getTypesList(widget.activeFilters['types']),
-                  'generations': _getGenerationList(widget.activeFilters['generations']),
-                  'searchName': widget.searchName.isEmpty ? '%%' : '%${widget.searchName.toLowerCase()}%',
-                }
-            );
+                  'generations':
+                      _getGenerationList(widget.activeFilters['generations']),
+                  'searchName': widget.searchName.isEmpty
+                      ? '%%'
+                      : '%${widget.searchName.toLowerCase()}%',
+                });
             break;
 
           case SortField.ability:
@@ -89,12 +89,43 @@ class _ListPokemonState extends State<ListPokemon> {
                 variables: {
                   'offset': pageKey * _pageSize,
                   'limit': _pageSize,
-                  'name': widget.currentSort.order == SortOrder.asc ? "asc" : "desc",
+                  'name': widget.currentSort.order == SortOrder.asc
+                      ? "asc"
+                      : "desc",
                   'types': _getTypesList(widget.activeFilters['types']),
-                  'generations': _getGenerationList(widget.activeFilters['generations']),
-                  'searchName': widget.searchName.isEmpty ? '%%' : '%${widget.searchName.toLowerCase()}%',
-                }
-            );
+                  'generations':
+                      _getGenerationList(widget.activeFilters['generations']),
+                  'searchName': widget.searchName.isEmpty
+                      ? '%%'
+                      : '%${widget.searchName.toLowerCase()}%',
+                });
+            break;
+
+          case SortField.bst:
+            options = QueryOptions(
+                document: gql(queryPokemonList),
+                fetchPolicy: FetchPolicy.cacheFirst,
+                variables: {
+                  'offset': pageKey * _pageSize,
+                  'limit': _pageSize,
+                  'types': _getTypesList(widget.activeFilters['types']),
+                  'generations':
+                      _getGenerationList(widget.activeFilters['generations']),
+                  'orderBy': [
+                    {
+                      "pokemon_v2_pokemonstats_aggregate": {
+                        "sum": {
+                          "base_stat": widget.currentSort.order == SortOrder.asc
+                              ? "asc"
+                              : "desc"
+                        }
+                      }
+                    }
+                  ],
+                  'searchName': widget.searchName.isEmpty
+                      ? '%%'
+                      : '%${widget.searchName.toLowerCase()}%',
+                });
             break;
 
           default:
@@ -105,12 +136,20 @@ class _ListPokemonState extends State<ListPokemon> {
                   'offset': pageKey * _pageSize,
                   'limit': _pageSize,
                   'types': _getTypesList(widget.activeFilters['types']),
-                  'generations': _getGenerationList(widget.activeFilters['generations']),
-                  'orderBy': [{widget.currentSort.field == SortField.id ? "id" : "name":
-                  widget.currentSort.order == SortOrder.asc ? "asc" : "desc"}],
-                  'searchName': widget.searchName.isEmpty ? '%%' : '%${widget.searchName.toLowerCase()}%',
-                }
-            );
+                  'generations':
+                      _getGenerationList(widget.activeFilters['generations']),
+                  'orderBy': [
+                    {
+                      widget.currentSort.field == SortField.id ? "id" : "name":
+                          widget.currentSort.order == SortOrder.asc
+                              ? "asc"
+                              : "desc"
+                    }
+                  ],
+                  'searchName': widget.searchName.isEmpty
+                      ? '%%'
+                      : '%${widget.searchName.toLowerCase()}%',
+                });
         }
       }
 
@@ -136,7 +175,6 @@ class _ListPokemonState extends State<ListPokemon> {
       } else {
         _pagingController.appendPage(pokemons, pageKey + 1);
       }
-
     } catch (error) {
       _pagingController.error = error;
     }
@@ -144,17 +182,14 @@ class _ListPokemonState extends State<ListPokemon> {
 
   @override
   Widget build(BuildContext context) {
-
     return PagedSliverGrid<int, dynamic>(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisSpacing: 8.0,
-        crossAxisSpacing: 8.0,
-        childAspectRatio: 0.85
-      ),
+          crossAxisCount: 2,
+          mainAxisSpacing: 8.0,
+          crossAxisSpacing: 8.0,
+          childAspectRatio: 0.85),
       builderDelegate: PagedChildBuilderDelegate(
         itemBuilder: (context, item, index) {
-
           return PokemonGridItem(pokemon: item);
         },
         firstPageProgressIndicatorBuilder: (context) => const Center(
@@ -172,31 +207,56 @@ class _ListPokemonState extends State<ListPokemon> {
 List<String>? _getTypesList(Set<String>? types) {
   if (types == null || types.isEmpty) {
     return [
-    'normal', 'fire', 'water', 'electric', 'grass', 'ice',
-    'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug',
-    'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy'
-  ];
+      'normal',
+      'fire',
+      'water',
+      'electric',
+      'grass',
+      'ice',
+      'fighting',
+      'poison',
+      'ground',
+      'flying',
+      'psychic',
+      'bug',
+      'rock',
+      'ghost',
+      'dragon',
+      'dark',
+      'steel',
+      'fairy'
+    ];
   }
   return types.map((type) => type.toLowerCase()).toList();
 }
 
 List<int>? _getGenerationList(Set<String>? generations) {
-  if (generations == null || generations.isEmpty){
+  if (generations == null || generations.isEmpty) {
     return [1, 2, 3, 4, 5, 6, 7, 8, 9];
   }
   return generations.map((generation) {
     final romanNumeral = generation.split(' ').last;
     switch (romanNumeral) {
-      case 'I': return 1;
-      case 'II': return 2;
-      case 'III': return 3;
-      case 'IV': return 4;
-      case 'V': return 5;
-      case 'VI': return 6;
-      case 'VII': return 7;
-      case 'VIII': return 8;
-      case 'IX': return 9;
-      default: return 1;
+      case 'I':
+        return 1;
+      case 'II':
+        return 2;
+      case 'III':
+        return 3;
+      case 'IV':
+        return 4;
+      case 'V':
+        return 5;
+      case 'VI':
+        return 6;
+      case 'VII':
+        return 7;
+      case 'VIII':
+        return 8;
+      case 'IX':
+        return 9;
+      default:
+        return 1;
     }
   }).toList();
 }
